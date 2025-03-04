038bf367848dc567c5cde44146fad049080ccc8b
heudiva <heudiva@gmail.com>
2025-03-04 15:43:00 +0700

    config

--- resources/views/website/components/navbar.blade.php
+++ resources/views/website/components/navbar.blade.php
@@ -21,11 +21,11 @@
         <a href=""><img class="w-14" src="https://flowbite.com/images/logo.svg" alt="wkschool-logo">
             <a class="pl-5 text-2xl dark:text-gray-200 font-kh-Moul font-bold" href="#">PROJECT-ASSIGNMENT</a>
         </div>
-    <div class="">
+    {{-- <div class="">
         @guest
          <a href="{{ route('login') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</a>
          <a href="{{ route('register') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</a>
          @endguest
-    </div>
+    </div> --}}
 </div>
 
