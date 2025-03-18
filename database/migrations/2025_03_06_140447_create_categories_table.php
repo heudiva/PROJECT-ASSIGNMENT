904e4d91c1ceec583bba15085112ae30de8afb57
Seyhaaa <Seyhadm21@gmail.com>
2025-03-18 11:45:13 +0700

    commit category_table

--- database/migrations/2025_03_06_140447_create_categories_table.php
+++ database/migrations/2025_03_06_140447_create_categories_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+             $table->id();
+            $table->string('name');
+            $table->string('name_khmer');
+            $table->string('description');
             $table->timestamps();
         });
     }
