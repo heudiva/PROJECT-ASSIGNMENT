4f6a0165675c74d6ca96ddaf7e9840cd9882815f
heudiva <heudiva@gmail.com>
2025-03-23 11:04:16 +0700

    edit login header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-
-    <title>@yield('title')</title>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="theme-color" content="#ffffff">
-
-    <script src="https://cdn.tailwindcss.com"></script>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+<x-guest-layout>
 <link rel="stylesheet" href="{{ asset('assets/css/auth.css') }}">
-
-<script>
-
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark');
-    } else {
-        document.documentElement.classList.remove('dark')
-    }
-</script>
-  </head>
-  <body class="bg-gray-50  body">
-
 
 @include('auth/animations')
 
@@ -70,9 +44,4 @@
     </div>
 </div>
 </main>
-
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/datepicker.min.js"></script>
-  </body>
-</html>
-
+</x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,35 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-
-    <title>@yield('title')</title>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="theme-color" content="#ffffff">
-
-    <script src="https://cdn.tailwindcss.com"></script>
-
-<link rel="stylesheet" href="{{ asset('assets/css/auth.css') }}">
-@vite(['resources/css/app.css','resources/js/app.js'])
-
-
-
-
-<script>
-
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark');
-    } else {
-        document.documentElement.classList.remove('dark')
-    }
-</script>
-  </head>
-  <body class="bg-gray-50 dark:bg-gray-800">
-
+<x-guest-layout>
+    <link rel="stylesheet" href="{{ asset('assets/css/auth.css') }}">
+    
     @include('auth/animations')
 
 <main class="main ">
@@ -90,10 +61,4 @@
 </div>
 </main>
 
-
-
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/datepicker.min.js"></script>
-  </body>
-</html>
+</x-guest-layout>
