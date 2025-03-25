3d6045d0264a73ebd90e02d2ee89e0f2c1653900
heudiva <heudiva@gmail.com>
2025-03-25 20:06:49 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-Route::get('/test', function () {
+
+Route::get('/testss', function () {
     return view('test');
 })->name('test');
 
